67f9bcbac7ffbe90361330807676220fe1555357
Pakaphon4 <pakaphon.pakaphon@gmail.com>
2025-09-24 15:59:00 +0700

    fiel

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const path = require("path");
 const port = 3000;
 const sqlite3 = require('sqlite3').verbose();
+const i18n = require("i18n");
 
 // Creating the Express server
 const app = express();
 
 // Connect to SQLite database
-let db = new sqlite3.Database('your-db-filename.db', (err) => {
+let db = new sqlite3.Database('project.db', (err) => {
     if (err) {
         return console.error(err.message);
     }
@@ -24,10 +25,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
-    ;
+    res.render('home');
 });
+
 
 
 app.listen(port, () => {
     console.log(`Starting server at port ${port}`);
 });
+
